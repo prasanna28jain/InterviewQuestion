cc8b81af952b433eef294ee6ac589962872abb6d
Prasanna <prasannajain28@gmail.com>
2023-10-11 23:41:55 +0530

    MY first Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//        Pizza basePizza = new Pizza(false);
+//        basePizza.addExtraTopping();
+//        basePizza.addExtraCheese();
+//        basePizza.getBill();
+
+        DeluxPizza dp = new DeluxPizza(true);
+        dp.getBill();
+    }
+}
